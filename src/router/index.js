f753d7d87277723b12ac0f02bea72d2a475e8f8c
Tohka <1324419148@qq.com>
2024-03-14 15:21:10 +0800

    添加路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,29 @@ const router = createRouter({
   routes: [
     {
       path:'/',
-      component:layout
+      component:layout,
+      children:[
+        {
+          path:"myday",
+          name:"myday",
+          component:()=>import('@/views/MyDay/index.vue')
+        },
+        {
+          path:"important",
+          name:"important",
+          component:()=>import('@/views/Important/index.vue')
+        },
+        {
+          path:"planned",
+          name:"planned",
+          component:()=>import('@/views/Planned/index.vue')
+        },
+        {
+          path:"inbox",
+          name:"inbox",
+          component:()=>import('@/views/Inbox/index.vue')
+        }
+      ]
     }
   ]
 })
